3b8dc44445d6e37fe694cdaec4c6b073327f4dc2
slipp <slipp@NB308.coinor.local>
2019-09-12 06:59:39 +0200

    properties path improved

--- slag-mail/slag-mail-app/src/main/java/de/slag/mail/MailProperties.java
+++ slag-mail/slag-mail-app/src/main/java/de/slag/mail/MailProperties.java
@@ -13,7 +13,19 @@ import org.apache.commons.lang3.StringUtils;
 
 public class MailProperties {
 
+	private static final String SSL = "ssl";
+
+	private static final String PORT = "port";
+
+	private static final String PASSWORD = "password";
+
+	private static final String USER = "user";
+
+	private static final String HOST = "host";
+
 	private static final String ACCOUNT = "account";
+
+	private static final String IMAP = "imap";
 
 	private static final String DOT = ".";
 
@@ -34,6 +46,10 @@ public class MailProperties {
 		this.properties = properties;
 	}
 
+	private String join(String... strings) {
+		return String.join(DOT, strings);
+	}
+
 	private Collection<String> collection(String key) {
 		final String property = properties.getProperty(key);
 		Objects.requireNonNull(property, "property not found: " + key);
@@ -41,8 +57,16 @@ public class MailProperties {
 		return Arrays.asList(split);
 	}
 
-	private boolean bool(String key) {
+	private boolean getBoolean(String key) {
 		return BooleanUtils.isTrue(Boolean.valueOf(properties.getProperty(key)));
+	}
+
+	private Integer getInteger(String key) {
+		String property = properties.getProperty(key);
+		if (StringUtils.isBlank(property)) {
+			return null;
+		}
+		return Integer.valueOf(property);
 	}
 
 	public Collection<String> getIds() {
@@ -50,27 +74,23 @@ public class MailProperties {
 	}
 
 	public String getHost(String id) {
-		return properties.getProperty(ACCOUNT + DOT + id + DOT + "host");
+		return properties.getProperty(join(ACCOUNT, id, IMAP, HOST));
 	}
 
 	public String getUser(String id) {
-		return properties.getProperty(ACCOUNT + DOT + id + DOT + "user");
+		return properties.getProperty(join(ACCOUNT, id, IMAP, USER));
 	}
 
 	public String getPassword(String id) {
-		return properties.getProperty(ACCOUNT + DOT + id + DOT + "password");
+		return properties.getProperty(join(ACCOUNT, id, IMAP, PASSWORD));
 	}
 
 	public Integer getPort(String id) {
-		String property = properties.getProperty(ACCOUNT + DOT + id + DOT + "port");
-		if (StringUtils.isBlank(property)) {
-			return null;
-		}
-		return Integer.valueOf(property);
+		return getInteger(join(ACCOUNT, id, IMAP, PORT));
 	}
 
 	public boolean isSsl(String id) {
-		return bool(ACCOUNT + DOT + id + DOT + "ssl");
+		return getBoolean(join(ACCOUNT, id, IMAP, SSL));
 	}
 
 }
