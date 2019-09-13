1a5f9e1d0a5948cabee22a35113eca058a74f14b
slipp <slipp@NB308.coinor.local>
2019-09-13 07:24:43 +0200

    ~ used MailCountOperation with filter to count Spam

--- slag-mail/slag-mail-app/src/main/java/de/slag/mail/ops/MailSpamCountOperation.java
+++ slag-mail/slag-mail-app/src/main/java/de/slag/mail/ops/MailSpamCountOperation.java
@@ -1,16 +1,22 @@
 package de.slag.mail.ops;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
 import org.apache.commons.lang3.StringUtils;
 
 import de.slag.mail.MailOperation;
-import de.slag.mail.model.MailFolder;
 import de.slag.mail.model.MailMessage;
 import de.slag.mail.model.MailStore;
 
 public class MailSpamCountOperation extends MailOperation<String> {
+
+	private Predicate<MailMessage> SPAM_FILTER = m -> {
+		final String subject = m.getSubject();
+		if (StringUtils.isEmpty(subject)) {
+			return false;
+		}
+		return subject.contains("SPAM");
+	};
 
 	public MailSpamCountOperation(MailStore mailStore) {
 		super(mailStore);
@@ -18,22 +24,7 @@ public class MailSpamCountOperation extends MailOperation<String> {
 
 	@Override
 	public String call() throws Exception {
-		MailStore mailStore = getMailStore();
-
-		final MailFolder inboxFolder = mailStore.getInboxFolder();
-		inboxFolder.open();
-		final Collection<MailMessage> messages = inboxFolder.getMessages().stream().map(m -> new MailMessage(m))
-				.collect(Collectors.toList());
-
-		final long spamCount = messages.stream().filter(m -> {
-			final String subject = m.getSubject();
-			if (StringUtils.isEmpty(subject)) {
-				return false;
-			}
-			return subject.contains("SPAM");
-		}).count();
-
-		return "Spam count: " + spamCount;
+		return new MailCountOperation(getMailStore(),SPAM_FILTER).callSafe();
 	}
 
 	@Override
