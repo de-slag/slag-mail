d92f149be7aca0c71f986e655d60f2e9fdd85d11
slipp <slipp@NB308.coinor.local>
2019-09-13 07:11:17 +0200

    MailFolder added: open and getMessages

--- slag-mail/slag-mail-app/src/main/java/de/slag/mail/model/MailFolder.java
+++ slag-mail/slag-mail-app/src/main/java/de/slag/mail/model/MailFolder.java
@@ -1,13 +1,17 @@
 package de.slag.mail.model;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import javax.mail.Folder;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Store;
 
 import de.slag.mail.MailException;
 
 public class MailFolder {
-	
+
 	private Folder folder;
 
 	public MailFolder(Store store, String folderName) {
@@ -17,10 +21,30 @@ public class MailFolder {
 			throw new MailException(e);
 		}
 	}
-	
+
 	public int getMessageCount() {
 		try {
 			return folder.getMessageCount();
+		} catch (MessagingException e) {
+			throw new MailException(e);
+		}
+	}
+
+	public void open() {
+		open(Folder.READ_ONLY);
+	}
+
+	public void open(int mode) {
+		try {
+			folder.open(mode);
+		} catch (MessagingException e) {
+			throw new MailException(e);
+		}
+	}
+
+	public Collection<Message> getMessages() {
+		try {
+			return Arrays.asList(folder.getMessages());
 		} catch (MessagingException e) {
 			throw new MailException(e);
 		}
