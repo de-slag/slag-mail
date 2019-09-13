c23531d17899787779fe96412f133d3cd6fadb2e
slipp <slipp@NB308.coinor.local>
2019-09-13 07:24:16 +0200

    ~MailCountOperation usable with filter

--- slag-mail/slag-mail-app/src/main/java/de/slag/mail/ops/MailCountOperation.java
+++ slag-mail/slag-mail-app/src/main/java/de/slag/mail/ops/MailCountOperation.java
@@ -1,17 +1,37 @@
 package de.slag.mail.ops;
 
+import java.util.function.Predicate;
+
 import de.slag.mail.MailOperation;
+import de.slag.mail.model.MailFolder;
+import de.slag.mail.model.MailMessage;
 import de.slag.mail.model.MailStore;
 
 public class MailCountOperation extends MailOperation<String> {
 
-	public MailCountOperation(MailStore mailStore) {
+	private Predicate<MailMessage> filter;
+
+	public MailCountOperation(MailStore mailStore, Predicate<MailMessage> filter) {
 		super(mailStore);
+		this.filter = filter;
+	}
+
+	public MailCountOperation(MailStore mailStore) {
+		this(mailStore, null);
 	}
 
 	@Override
 	public String call() throws Exception {
-		return "message count: " + getMailStore().getInboxFolder().getMessageCount();
+		final long messageCount;
+		final MailFolder inboxFolder = getMailStore().getInboxFolder();
+		if (filter == null) {
+			messageCount = inboxFolder.getMessageCount();
+		} else {
+			inboxFolder.open();
+			messageCount = inboxFolder.getMessages().stream().map(m -> new MailMessage(m))
+					.filter(filter).count();
+		}
+		return "message count: " + messageCount;
 	}
 
 	@Override
