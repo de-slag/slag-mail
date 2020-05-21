e87b7926377f7be6f51c3251c42b7c2a79cfbfaf
info <info@CALYPSO.fritz.box>
2020-05-21 16:11:39 +0200

    init mail webapp

---
+++ slag-mail/slag-mail-webapp/src/main/java/de/slag/mail/webapp/MailController.java
@@ -0,0 +1,22 @@
+package de.slag.mail.webapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MailController {
+
+	public String getState() {
+		return "mailController";
+	}
+
+	
+}
