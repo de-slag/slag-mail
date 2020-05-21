44a68eaeaca177e25dfb419fa615421329a39bc4
info <info@CALYPSO.fritz.box>
2020-05-21 22:02:02 +0200

    savable key value
    still with bug

--- slag-mail/slag-mail-webapp/src/main/java/de/slag/mail/webapp/MailConfigController.java
+++ slag-mail/slag-mail-webapp/src/main/java/de/slag/mail/webapp/MailConfigController.java
@@ -1,5 +1,13 @@
 package de.slag.mail.webapp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -7,9 +15,80 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class MailConfigController {
 
-	public String getState() {
-		return "mailConfigController";
+	private String key;
+
+	private String value;
+	
+	private final Map<String, String> configuration = new HashMap<>();
+
+	private final List<ConfigEntry> configurationEntries = new ArrayList<>();
+	
+	@PostConstruct
+	public void init() {
+		resetConfig();
 	}
 
-	
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getState() {
+		return "nix";
+	}
+
+	public List<ConfigEntry> getConfigurationEntries() {
+		return Collections.emptyList();
+	}
+
+	public void saveKeyValue() {
+		configuration.put(key, value);
+		key = null;
+		value = null;
+		resetConfig();
+	}
+	public void resetConfig() {
+		configurationEntries.clear();
+		configurationEntries.addAll(configuration.keySet().stream().map(key -> {
+			ConfigEntry configEntry = new ConfigEntry();
+			configEntry.setKey(key);
+			configEntry.setValue(configuration.get(key));
+			return configEntry;
+		}).collect(Collectors.toList()));
+	}
+
+	public class ConfigEntry {
+		private String key;
+		private String value;
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+	}
+
 }
---
+++ slag-mail/slag-mail-webapp/src/main/java/de/slag/mail/webapp/MailConfigController2.java
@@ -0,0 +1,15 @@
+package de.slag.mail.webapp;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MailConfigController2 {
+
+	public String getState() {
+		return "mailConfigController";
+	}
+
+	
+}
